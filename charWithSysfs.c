4661abc95ad50a0faa77b5f02b52549fb27326cf
AdvLinux <advlinux@advlinux-VirtualBox.(none)>
2014-12-08 18:42:39 +0800

    update charwithsys.c

--- charWithSysfs.c
+++ charWithSysfs.c
@@ -1,5 +1,9 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/sysfs.h>
+#include <linux/device.h> /* class_create */
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Quentin <lee.rhapsody AT gmail.com>");
@@ -10,15 +14,17 @@ static int task_major = 0; /* dynamic allocation */
 static struct cdev task_cdev;
 static int dev_num = 1;
 
+static struct class *task_class = NULL;
+
 static int task_open(struct inode *inode, struct file *file)
 {
-	printk(KERN_DEBUG"task_open: major number %d, minor number = %d\n", imajor(inode), iminjor(inode));
+	pr_debug("task_open: major number %d, minor number = %d\n", imajor(inode), iminjor(inode));
 	return 0;
 }
 
 static int task_release(struct inode *inode, struct file *file)
 {
-	printk(KERN_DEBUG"task_release: major number %d, minor number = %d\n", imajor(inode), iminjor(inode));
+	pr_debug("task_release\n");
 	return 0;
 }
 
@@ -29,27 +35,41 @@ struct file_operations task_fops = {
 };
 static int task_init(void)
 {
-	dev_t dev = MKDEV(task_major, 0);
-	int alloc_ret = 0, cdev_ret = 0;
+	dev_t dev;
+	int ret = 0;
 	
-	printk(KERN_DEBUG"init simple Linux driver");	
-	alloc_ret = alloc_chrdev_region(&dev, 0, task_cdev, DRIVER_NAME);       	
-	if (alloc_ret)
-		goto error;
-	task_major = MAJOR(dev);
+	pr_debug("init simple Linux driver\n");	
+	ret = alloc_chrdev_region(&dev, 0, dev_num, DRIVER_NAME);       	
+	if (ret < 0) {
+		pr_debug("fail to alloc char device\n");
+		return ret;
+	}
 	cdev_init(&task_cdev, &task_fops);
-	cdev_ret = cev_add(task_cdev, MKDEV(task_major, 0), dev_num);
-    	if (cdev_ret)
+	task_cdev.owner = THIS_MODULE;
+
+	ret = cev_add(&task_cdev, dev, dev_num);
+    	if (cdev_ret) {
+		pr_debug("fail to add char device\n");
 		goto error;
+	}
+	/* sysfs */
+	task_class = class_create(THIS_MODULE, DRIVER_NAME);
+	if (IS_ERR(task_class)) {
+		pr_debug("fail to create class\n");
+		return -1;	
+	}
+	
+	device_create(task_class, NULL, dev, NULL, "mydev%d", 0);
 	return 0;	
 
 error:
+	task_exit();
 	
 }
 
 static void task_exit(void)
 {
-	printk(KERN_DEBUG"exit\n");
+	pr_debug("exit\n");
 }
 
 module_init(task_init);
